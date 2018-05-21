529fb690bda03fa6eb68274a9197cc63d3adf05a
Andrii <andrii@adsquare.com>
2018-05-21 20:11:49 +0200

    added inversion count to merge sort

--- src/main/scala/org/ankin/algorithms/MergeSort.scala
+++ src/main/scala/org/ankin/algorithms/MergeSort.scala
@@ -11,46 +11,57 @@ object MergeSort {
   private val scheduler =
     new DynamicVariable[DefaultTaskScheduler](new DefaultTaskScheduler)
 
-  def sort(array: IndexedSeq[Int]): IndexedSeq[Int] = {
+  def sort(array: Array[Int]): (Long, Array[Int]) = {
     mergeSort(array, 0, array.length)
   }
 
-  private def mergeSort(list: IndexedSeq[Int], from: Int, to: Int): IndexedSeq[Int] = {
+  private def mergeSort(list: Array[Int], from: Int, to: Int): (Long, Array[Int]) = {
     if (to - from <= 1) {
-      Vector(list(from))
+      0L -> Array(list(from))
     } else {
       val middle = (from + to) / 2
-      val first = task {
+
+      val firstTask = task {
         mergeSort(list, from, middle)
       }
-      val second = task {
+      val (secondInvCount, secondArr) = task {
         mergeSort(list, middle, to)
+      }.join()
+
+      val (firstInvCount, firstArr) = firstTask.join()
+      merge(firstArr, secondArr) match {
+        case (splitInvCount, mergedArr) => (firstInvCount + secondInvCount + splitInvCount -> mergedArr)
       }
-      merge(first.join(), second.join())
 
     }
   }
 
-  private def merge(one: IndexedSeq[Int], two: IndexedSeq[Int]): IndexedSeq[Int] = {
-    val iter = new Iterator[Int] {
-      var oneIdx = 0
-      var twoIdx = 0
-      override def hasNext: Boolean = oneIdx != one.length || twoIdx != two.length
-      override def next(): Int = if (oneIdx == one.length) {
+  private def merge(one: Array[Int], two: Array[Int]): (Long, Array[Int]) = {
+    var inversionsCount = 0l
+    val result = new Array[Int](one.length + two.length)
+
+    var oneIdx = 0
+    var twoIdx = 0
+    var resIdx = 0
+
+    while (oneIdx != one.length || twoIdx != two.length) {
+      if (oneIdx == one.length) {
+        result(resIdx) = two(twoIdx)
         twoIdx = twoIdx + 1
-        two(twoIdx - 1)
       } else if (twoIdx == two.length) {
+        result(resIdx) = one(oneIdx)
         oneIdx = oneIdx + 1
-        one(oneIdx - 1)
       } else if (one(oneIdx) <= two(twoIdx)) {
+        result(resIdx) = one(oneIdx)
         oneIdx = oneIdx + 1
-        one(oneIdx - 1)
       } else {
+        inversionsCount = inversionsCount + one.length - oneIdx
+        result(resIdx) = two(twoIdx)
         twoIdx = twoIdx + 1
-        two(twoIdx - 1 )
       }
+      resIdx = resIdx + 1
     }
-    iter.toIndexedSeq
+    inversionsCount -> result
   }
 
   private class DefaultTaskScheduler {
@@ -73,7 +84,9 @@ object MergeSort {
   }
 
   def main(args: Array[String]): Unit = {
-    sort(Vector(2, 3, 2, 4, 1)).foreach(println(_))
+    sort(Array(2, 4, 1, 3, 5)) match {
+      case (inversions, sorted) => println(s"Inversions: $inversions, sorted result: ${sorted.mkString(",")}")
+    }
   }
 
 }
